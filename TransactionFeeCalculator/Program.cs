525d21da85087ff6be3ecaf077c4f76f6998a04a
James Morgan <MorganJay@users.noreply.github.com>
2021-04-04 22:33:15 +0100

    Refactored classes and helper methods. Placed fees config file in project and reading from it

--- TransactionFeeCalculator/Program.cs
+++ TransactionFeeCalculator/Program.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
 
 namespace TransactionFeeCalculator
 {
@@ -10,8 +9,8 @@ namespace TransactionFeeCalculator
     {
         private static void Main(string[] args)
         {
-            var jsonFilePath = @"C:\Users\JamesMorgan\Desktop\fees.config.json";
-            var feeConfig = GetFeeConfig(jsonFilePath);
+            var jsonFilePath = "fees.config.json";
+            var feeConfig = CalculatorFunctions.GetFeeConfig(jsonFilePath);
             bool pass;
             Console.WriteLine("Welcome, how much would you like to transfer? ");
             pass = decimal.TryParse(Console.ReadLine(), out decimal transaction);
@@ -24,43 +23,11 @@ namespace TransactionFeeCalculator
                     pass = decimal.TryParse(Console.ReadLine(), out transaction);
                 } while (!pass);
             }
-            var transferCharge = CalculateTransactionFee(transaction, feeConfig);
+            var transferCharge = CalculatorFunctions.CalculateTransactionFee(transaction, feeConfig);
 
             Console.WriteLine($"You will be charged {transferCharge} for a transfer of {transaction}. \nThank you for banking with us");
 
             Console.ReadLine();
         }
-
-        public static decimal CalculateTransactionFee(decimal amount, List<Fee> feesConfig)
-        {
-            decimal charge = 0.0m;
-            foreach (var fee in feesConfig)
-            {
-                if (amount >= fee.MinAmount && amount <= fee.MaxAmount)
-                {
-                    charge = fee.FeeAmount;
-                }
-            }
-            return charge;
-        }
-
-        public static List<Fee> GetFeeConfig(string path)
-        {
-            string json = File.ReadAllText(path);
-            var fees = JsonConvert.DeserializeObject<Root>(json);
-            return fees.Fees;
-        }
-    }
-
-    public class Fee
-    {
-        public int MinAmount { get; set; }
-        public int MaxAmount { get; set; }
-        public int FeeAmount { get; set; }
-    }
-
-    public class Root
-    {
-        public List<Fee> Fees { get; set; }
     }
 }
